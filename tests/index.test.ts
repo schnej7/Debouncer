6470b0846e1e461dc179fab23bb576416d06bf80
Jerry Schneider <schnej7@gmail.com>
2025-05-02 15:31:43 -0400

    Added a legit test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,4 +1,4 @@
-import { describe, it } from 'node:test';
+import { describe, it, mock } from 'node:test';
 import assert from 'node:assert';
 
 import Debouncer from '../src/index.ts';
@@ -13,5 +13,26 @@ describe('NPM Package', () => {
 describe('Debouncer Class', () => {
   it('should be a function', () => {
     assert.strictEqual(typeof Debouncer, 'function');
+  });
+
+  it('should debounce a function', () => {
+    const DEBOUNCE_TIMEOUT = 1000;
+    mock.timers.enable();
+
+    const mockFn = mock.fn();
+    const debouncer = new Debouncer(mockFn, DEBOUNCE_TIMEOUT);
+
+    // Call debounce 3 times
+    debouncer.debounce();
+    debouncer.debounce();
+    debouncer.debounce();
+
+    // mockFn should only get called once
+    assert.deepStrictEqual(mockFn.mock.calls.length, 1);
+
+    mock.timers.tick(DEBOUNCE_TIMEOUT);
+
+    // After timeout has elapsed, mockFn should get called one more time
+    assert.deepStrictEqual(mockFn.mock.calls.length, 2);
   });
 });
